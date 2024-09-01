130f356103cef31662c568c13e511bdf0517c691
Luis Enrique Campos juarez <LenriqueJ2558gmail.com>
2024-09-01 18:51:10 -0500

    actulizado

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://martl7.github.io/February-Declaration/",
+  base: "/",
   build: {
     outDir: 'dist', // Asegúrate de que este sea el nombre del directorio de salida
   },
